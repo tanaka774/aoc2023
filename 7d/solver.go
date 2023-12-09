abb001fb4e10fd1a3e6c6a6b2ffd5aa67d62a855
gram <gram@localhost.localdomain>
2023-12-09 20:44:09 +0900

    day7-2 solved

--- 7d/solver.go
+++ 7d/solver.go
@@ -6,15 +6,9 @@ import (
 	"slices"
 )
 
-const (
-	// cards   = []string{"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"}
-	// cards   = []byte{'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'}
-	drawing = 5
-)
-
 func main() {
 	// ans1() // ans:253933213
-	ans2() //:ans
+	ans2() // ans:253473930
 }
 
 func ans1() {
@@ -118,7 +112,6 @@ func calcLabelPoint(str string) int {
 	for i, ch := range str {
 		for j, card := range cards {
 			if ch == card {
-				// point += (len(str) - i) * (len(cards) - j)
 				point += int(math.Pow(float64(len(cards)), float64(len(str)-1-i))) * (len(cards) - j)
 			}
 		}
@@ -133,7 +126,6 @@ func calcTypePoint2(str string) int {
 	jCount := 0
 	for i := range sorted {
 		if 'J' == sorted[i] {
-			fmt.Println(sorted)
 			jCount++
 			continue
 		}
@@ -154,32 +146,32 @@ func calcTypePoint2(str string) int {
 		}
 	}
 
-	if len(sames) == 0 && jCount >= 1 {
+	if jCount == 5 {
+		sames = append(sames, 5)
+	} else if len(sames) == 0 && jCount >= 1 {
 		sames = append(sames, 1+jCount)
-		fmt.Println(sames)
 	} else if jCount >= 1 {
 		maxIndex := slices.Index(sames, slices.Max(sames))
 		sames[maxIndex] += jCount
-		fmt.Println(sames)
 	}
 
 	if len(sames) == 0 {
-		return 200000000
+		return 20000000
 	} else if sames[0] == 5 {
-		return 800000000
+		return 80000000
 	} else if sames[0] == 4 {
-		return 700000000
+		return 70000000
 	} else if len(sames) >= 2 && sames[0]+sames[1] == 5 {
 		// fullhouse
-		return 600000000
+		return 60000000
 	} else if sames[0] == 3 {
-		return 500000000
+		return 50000000
 	} else if len(sames) >= 2 && sames[0]+sames[1] == 4 {
 		// two pair
-		return 400000000
+		return 40000000
 	} else {
 		// one pair
-		return 300000000
+		return 30000000
 	}
 }
 
@@ -189,29 +181,9 @@ func calcLabelPoint2(str string) int {
 	for i, ch := range str {
 		for j, card := range cards {
 			if ch == card {
-				// point += (len(str) - i) * (len(cards) - j)
 				point += int(math.Pow(float64(len(cards)), float64(len(str)-1-i))) * (len(cards) - j)
 			}
 		}
 	}
 	return point
 }
-
-// func compareLabel(str1 string, str2 string) bool {
-// 	var res bool
-// 	for i := 0; i < drawing; i++ {
-// 		if str1[i] == str2[i] {
-// 			continue
-// 		}
-// 		for _, card := range cards {
-// 			if string(str1) == card {
-// 				res = true
-// 			} else if string(str2) == card {
-// 				res = false
-// 			} else {
-// 				continue
-// 			}
-// 		}
-// 	}
-// 	return res
-// }
--- readme.md
+++ readme.md
@@ -3,13 +3,13 @@ Go
 ## editor
 neovim(lazyvim)
 ## result
-17/18
+18/18
 ### solved
 1-1,2 2-1,2 3-1,2 4-1,2 5-1,2 6-1,2 7-1 8-1,2 9-1,2
 #### solved with some advice
-8-2
+7-2(`JJJJJ`), 8-2(ZtoZ)
 ### unsolved
-7-2
+
 
 ## structure
 ???  
